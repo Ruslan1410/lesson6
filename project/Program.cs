668069353f025a1281af3c6b80c19d46272a34e3
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-04 20:36:25 +0300

    project 1

---
+++ project/Program.cs
@@ -0,0 +1,24 @@
+﻿// задайте массив символов (тип char []).
+// создайте строку из симполов этого массива.
+
+// указание
+// конструктор строки вида string(char []) не использовать.
+// [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+string GetStringFromCharArray (char [] chars){
+    string res ="";
+    foreach (char el in chars){
+        res += el;
+    }
+    return res;
+}
+
+char [] chars = {'a','b','c','e'};
+string str = GetStringFromCharArray (chars);
+Console.WriteLine(str);
+
+
+int [] numb = {1,2,3,4,5};
+foreach (int i in numb){
+    
+}
