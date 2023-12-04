f39c44243212168addc898f1c2990d9de35ea78f
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-04 21:09:20 +0300

    project 3

--- project/Program.cs
+++ project/Program.cs
@@ -46,3 +46,31 @@
 // string str = "Hello world!";
 // char [] chars = StringToCharArray(str);
 // ShowCharsArr(chars);
+
+// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+
+int GetCountVovelsInString(string str)
+{
+    string vovels = "aeiou"; 
+    int count = 0;
+    foreach(char elem in str)
+    {
+        foreach(char vol in vovels)
+        {
+            if(vol == elem)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+
+
+string str = "Hello world";
+int chars = GetCountVovelsInString(str);
+Console.WriteLine ($"Vovels in {str} is {chars}");
